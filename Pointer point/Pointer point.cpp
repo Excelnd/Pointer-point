efeea1f429eb125cc6ae5b99266b2cf7d025fa97
ihsan <ihsanmpm@gmail.com>
2017-12-08 20:37:36 +0530

    Add project files.

---
+++ Pointer point/Pointer point.cpp
@@ -0,0 +1,23 @@
+// Pointer point.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int firstvalue, secondvalue;
+	int * mypointer;
+
+	mypointer = &firstvalue;
+	*mypointer = 10;
+	mypointer = &secondvalue;
+	*mypointer = 20;
+
+	cout << "firstvalue is " << firstvalue << '\n';
+	cout << "secondvalue is " << secondvalue << '\n';
+
+    return 0;
+}
+
